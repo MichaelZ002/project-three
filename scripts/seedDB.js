9ea122f7b13147a9e956ddf4b5fc88475681dde3
Arpita K Sau <arpitakaran@gmail.com>
2020-10-02 11:29:58 -0500

    updated-file-structure

---
+++ scripts/seedDB.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/"
+);
+
+const seed = [
+  {
+  }
+];
