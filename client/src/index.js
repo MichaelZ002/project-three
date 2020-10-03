586ece372cdcd579fa17daaf31b0ea4cadf32e19
Brianna Bullock <brianna.bullock16@gmail.com>
2020-10-03 12:27:17 -0500

    fixed api key undefined, added diy to search queries

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import 'bootstrap/dist/css/bootstrap.min.css';
+require('dotenv').config()
+
+
 
 ReactDOM.render(
   <BrowserRouter>
