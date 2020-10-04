919d069239ad7e66503f451c0562a4783bee2551
Arpita K Sau <arpitakaran@gmail.com>
2020-10-04 01:35:13 -0500

    UI-update

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Grid, InputAdornment, TextField, Typography, FormControlLabel, Checkbox, Card, CardActionArea, CardContent, CardMedia} from "@material-ui/core";
+import { Button, Grid, InputAdornment, TextField, Typography, FormControlLabel, Checkbox, Card, CardActionArea, CardContent, CardMedia, Paper, Box} from "@material-ui/core";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import { AccountCircle, LockRounded } from "@material-ui/icons";
 import Image from "../../images/bg.jpg"
@@ -9,19 +9,40 @@ import Typed from "react-typed";
 const useStyles = makeStyles((theme) => ({
 
     title: {
-        color:"yellow"
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
+        color:"tan"
     },
     subcontainerRight: {
         backgroundColor: "#bcc2d7"
     },
-    cardContainer: {
-        margin: "3rem",
-        margin: "5rem auto"
-    }
+    loginBackground: {
+        justify: "center",
+        minHeight: "30vh",
+        padding: "50px"
+      }
 }))
+
+const InputField = withStyles({
+    root: {
+        "& label.Mui-focused" : {
+            color: "tan",
+        },
+        "& label": {
+            color: "teal",
+        },
+        "& .MuiOutlinedInput-root" : {
+            "& fieldset" : {
+                borderColor: "gray",
+            },
+            "&:hover fieldset" : {
+                borderColor: "blue",
+            },
+            "& .Mui-focused fieldset": {
+                borderColor: "tan",
+            },
+        },
+
+    },
+})(TextField);
 
 
 // export default class Login extends Component {
@@ -35,11 +56,8 @@ const useStyles = makeStyles((theme) => ({
                     <Grid item xs={12} sm={6}>
                         <img src={Image} style={{ width: "100%", height: "100%", objectFit: "cover"}} alt="background"/>
                     </Grid>
-                    <Grid container className={classes.subcontainerRight} item xs={12} sm={6} 
-                    alignItems="center" 
-                    direction="column" 
-                    justify="space-between" 
-                    style={{padding: 10}}>
+                    <Grid container className={classes.subcontainerRight} item xs={12} sm={6} alignItems="center" direction="column" justify="space-between" style={{padding: 10}}>
+                    <Paper className={classes.loginBackground} variant="elevation" elevation={4}>
                         <div />
                         <div style={{ display: "flex", flexDirection: "column", maxWidth: 400, minWidth: 300}}>
                            <Grid container justify="center" >
@@ -48,17 +66,17 @@ const useStyles = makeStyles((theme) => ({
                            </Grid>
                            <br />
                            <Grid container justify="center" >
-                           <Typography component="h1" variant="h5" style={{color:"white"}} >Sign In
+                           <Typography component="h1" variant="h5" style={{color:"#bcc2d7"}} >Sign In
                            </Typography>
                            </Grid>
-                           <TextField 
+                           <InputField 
                            label="Username" 
                            margin="normal" 
                            variant="outlined"
                            id="username"
                            InputProps={{ startAdornment: <InputAdornment position="start"><AccountCircle /></InputAdornment>}}
                            />
-                           <TextField 
+                           <InputField 
                            type="password" 
                            variant="outlined"
                            label="Password" 
@@ -71,21 +89,16 @@ const useStyles = makeStyles((theme) => ({
             label="Remember me"
           />
                            <div style={{ height: 20}} />
-                           <Button 
-                           color="primary" 
-                           type="submit"
-                           variant="contained"
-                           className={classes.submit}
-                           >Log in
-                           </Button>
+                           <Button color="primary" variant="contained" style={{backgroundColor:"#21b6ae"}}>Log In</Button>
                            <div style={{ height: 20}} />
                            <a href="/sign-up">Not registered, Sign up</a>
                            
                         </div>
                         <div />
-                        
+                        </Paper>
                     </Grid>
-                </Grid>
+               
+              </Grid>
 
             </div>
            
