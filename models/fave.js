b3268b6fa334942880addf0c3b691118cbf698fc
Ben de Garcia <degarcia.ben@gmail.com>
2020-10-14 19:40:44 -0500

    minor formatting

--- models/fave.js
+++ models/fave.js
@@ -1,19 +1,16 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const UserFave =  new Schema({
-    UID: {
-        type: String,
-        required: true
-    },
-    faveVids: {
-        type: Array,
-    }
-})
+const UserFave = new Schema({
+  UID: {
+    type: String,
+    required: true,
+  },
+  faveVids: {
+    type: Array,
+  },
+});
 
-const Fave = mongoose.model("Fave", UserFave)
+const Fave = mongoose.model("Fave", UserFave);
 
 module.exports = Fave;
-
-
-
