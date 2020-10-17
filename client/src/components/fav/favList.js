c21d99354b67c6d28670c8745df4e5b7d0cf99a8
Ben de Garcia <degarcia.ben@gmail.com>
2020-10-17 12:38:17 -0500

    fixed memory leak

--- client/src/components/fav/favList.js
+++ client/src/components/fav/favList.js
@@ -17,12 +17,12 @@ export default (props) => {
   const history = useHistory();
 
   useEffect(() => {
-    if (uHasData.length < 1 && props.user) {
+    if (uHasData.length <= 1 && props.user) {
       console.log("Hey! You found some data!");
       console.log(props.user.uid);
       grabUserData(props.user.uid);
     }
-  });
+  }, []);
 
   function createNewProject(favID, uid) {
     console.log(`${favID} ${uid} I AM TRYING`);
@@ -50,7 +50,7 @@ export default (props) => {
 
   function destroyUserData(favID) {
     API.deleteUserData(props.user.uid, favID)
-      .then((res) => grabUserData())
+      .then((res) => grabUserData(props.user.uid))
       .catch((err) => console.log(err));
   }
 
